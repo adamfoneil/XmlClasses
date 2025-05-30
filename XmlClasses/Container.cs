cb9ad196f1aed0bcb240e2cc43faae4f802dd0d6
Adam O'Neil <adamosoftware@gmail.com>
2025-05-30 07:06:28 -0400

    stubbed out interface

--- XmlClasses/Container.cs
+++ XmlClasses/Container.cs
@@ -1,6 +1,22 @@
-﻿namespace XmlClasses;
+﻿using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
 
-public class Container
+namespace XmlClasses;
+
+public class Container : IXmlSerializable
 {
 	public Item[] Items { get; set; } = [];
+
+	public XmlSchema? GetSchema() => null;
+
+	public void ReadXml(XmlReader reader)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void WriteXml(XmlWriter writer)
+	{
+		throw new NotImplementedException();
+	}
 }
