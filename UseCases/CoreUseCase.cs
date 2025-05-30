9988fc2b5479463d56de247a7244261aa5b53091
Adam O'Neil <adamosoftware@gmail.com>
2025-05-30 07:05:42 -0400

    something else

--- UseCases/CoreUseCase.cs
+++ UseCases/CoreUseCase.cs
@@ -13,13 +13,7 @@ public sealed class CoreUseCase
 		var container = (XmlClasses.Container)serializer.Deserialize(GetStream("Sample.xml")) ?? throw new Exception("Deserialization failed");
 
 		var itemNames = container.Items.Select(i => i.Name).ToArray();
-		Assert.IsTrue(itemNames.SequenceEqual(["This", "That", "Hello"]);
-	}
-
-	private static string GetContent(string resourceName)
-	{
-		using var stream = GetStream(resourceName);
-		return new StreamReader(stream).ReadToEnd();
+		Assert.IsTrue(itemNames.SequenceEqual(["This", "That", "Hello"]));
 	}
 
 	private static Stream GetStream(string resourceName) => Assembly.GetExecutingAssembly().GetManifestResourceStream($"UseCases.Resources.{resourceName}") ?? throw new Exception("stream not found");
